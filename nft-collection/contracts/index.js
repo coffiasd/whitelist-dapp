5f8af33b04f914538d8e483ab24c4c391e6e7bd0
coffiasd <759291502@qq.com>
2022-05-22 06:49:27 +0000

    add nft website

---
+++ nft-collection/contracts/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x15cdaea0f60e083d5da74bcd8a27531af5ea91cb";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
